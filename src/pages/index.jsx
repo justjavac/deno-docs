5b47a3267e652b7017e325b2f7095c3d54a7609e
迷渡 <justjavac@gmail.com>
2023-11-01 15:23:39 +0800

    fmt

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ function Card({ title, href, children }) {
         className="font-bold inline-block hover:opacity-80"
         href={href}
       >
-        {title} Docs &gt;
+        {title} 文档 &gt;
       </a>
     </div>
   );
@@ -21,8 +21,8 @@ function Card({ title, href, children }) {
 export default function Home() {
   return (
     <Layout
-      title={"Deno: the easiest, most secure JavaScript runtime"}
-      description="Reference documentation for the Deno runtime and Deno Deploy"
+      title={"Deno：最简单、最安全的 JavaScript 运行时"}
+      description="Deno 运行时和 Deno Deploy 的参考文档"
     >
       <div className="flex flex-col px-8 pt-6 md:mt-12 md:items-center md:justify-center md:flex-row gap-0 md:gap-16 max-w-[1200px] mx-auto">
         <div className="pb-16 align-middle md:pb-0">
@@ -32,21 +32,21 @@ export default function Home() {
               alt="Deno logo"
               src="/deno-looking-up.svg"
             />
-            <h1 className="text-4xl md:text-5xl">Deno Docs</h1>
+            <h1 className="text-4xl md:text-5xl">Deno 文档</h1>
           </div>
           <div className="flex flex-col items-start gap-8 md:grid md:grid-cols-3 md:grid-flow-col md:items-stretch">
             <Card title="Deno Runtime" href="/runtime/manual">
-              Language runtime for TypeScript and JavaScript with a browser-like
-              programming environment. Features built-in dev tools, powerful
-              platform APIs, and native support for TypeScript and JSX.
+              用于 TypeScript 和 JavaScript
+              的语言运行时，具有类似浏览器的编程环境。具备内置开发工具、强大的平台
+              AP I以及对 TypeScript 和 JSX 的原生支持。
             </Card>
             <Card title="Deno Deploy" href="/deploy/manual">
-              Serverless JavaScript platform. Supports Deno platform APIs and
-              Node.js / npm modules. Runs on a fast global edge network.
+              Serverless JavaScript 平台。支持 Deno 平台API和 Node.js/npm
+              模块。运行在快速的全球边缘网络上。
             </Card>
             <Card title="Deno KV" href="/kv/manual">
-              Key/value database built in to the Deno runtime. Simple API, works
-              with zero configuration on Deno Deploy.
+              Deno 运行时内置键值数据库。具有简单的 API，在 Deno Deploy
+              上无需任何配置即可使用。
             </Card>
           </div>
         </div>
